6e84bfbc3d953221635e7d7bc6c613a4c68a85aa
yansongda <me@yansongda.cn>
2018-01-18 09:51:36 +0800

    fix #3

--- src/helpers.php
+++ src/helpers.php
@@ -12,6 +12,6 @@ if (!function_exists('parsehtml')) {
      */
     function parsehtml($expression)
     {
-        return resolve('parsehtml')->convert($expression);
+        return app('parsehtml')->convert($expression);
     }
 }
